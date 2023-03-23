0a4fbefaf8be05d206ac908df9f3e428f7059f5e
Francesco Medina <s280620@studenti.polito.it>
2023-03-24 00:06:42 +0100

    more es

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,14 +2,20 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
+//UTILE
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(data.clone());
 
     string_uppercase(&data);
+
+    // let mut data1 = "Rust is great!".to_string();
+    //
+    // get_char(data1.clone());
+    //
+    // string_uppercase(&mut data1);
 }
 
 // Should not take ownership
@@ -19,7 +25,21 @@ fn get_char(data: String) -> char {
 
 // Should take ownership
 fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+    &data.to_uppercase();
 
     println!("{}", data);
 }
+
+//-------------------------VARIANT
+
+// // Should not take ownership
+// fn get_char(data: String) -> char {
+//     data.chars().last().unwrap()
+// }
+//
+// // Should take ownership
+// fn string_uppercase(data: &mut String) {
+//     &data.to_uppercase();
+//
+//     println!("{}", data);
+// }
